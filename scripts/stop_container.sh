e640c90cfa2ef7b4be08a95819deb1183fc6f7f7
spandanapriya-nadipineni <52268832+spandanapriya-nadipineni@users.noreply.github.com>
2024-04-29 13:00:48 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 set -e
-containerid=$(docker ps | awk 'NR==2 {print $1}')
 
-# Check if container ID is not empty
-if [ -n "$containerid" ]; then
-    # Stop the container
-    docker rm -f "$containerid"
-    echo "Container with ID $containerid stopped."
-else
-    echo "No running containers found."
-fi
+# Stop the running container (if any)
+echo "Hi"
